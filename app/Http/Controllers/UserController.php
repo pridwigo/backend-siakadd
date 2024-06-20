5be20d4c30e764216f60b5ea63306f7f5c914c5d
pridwigo <primantorodev@outlook.com>
2024-06-20 23:00:01 +0700

    integrate data user and paginate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view("pages.users.index");
+        $users = DB::table("users")
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%'.$name.'%');
+            })
+            ->select('id', 'name', 'email', 'phone', DB::raw('DATE_FORMAT(created_at,"%d %M %Y")as created_at'))
+            ->paginate(10);
+        return view("pages.users.index", compact("users"));
     }
 
     /**
---
+++ resources/views/pages/users/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.app')
+
+@section('title', 'Users')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>All Users</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Users</a></div>
+                    <div class="breadcrumb-item">All Users</div>
+                </div>
+            </div>
+            <div class="section-body">
+
+
+
+                <div class="row mt-4">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>All Users</h4>
+
+                                <div class="section-header-button">
+                                    <a href="features-post-create.html" class="btn btn-primary">New User</a>
+                                </div>
+                            </div>
+                            <div class="card-body">
+
+                                <div class="float-right">
+                                    <form method="GET", action="{{ route('user.index') }}">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" placeholder="Search" name="name">
+                                            <div class="input-group-append">
+                                                <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div class="clearfix mb-3"></div>
+
+                                <div class="table-responsive">
+                                    <table class="table-striped table">
+                                        <tr>
+
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>Created At</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        @foreach ($users as $user)
+                                            <tr>
+                                                <td>
+                                                    {{ $user->name }}
+                                                </td>
+                                                <td>
+                                                    {{ $user->email }}
+                                                </td>
+                                                <td>
+                                                    {{ $user->phone }}
+                                                </td>
+                                                <td>
+                                                    {{ $user->created_at }}
+                                                </td>
+                                                <td>
+                                                    Edit
+                                                </td>
+                                            </tr>
+                                        @endforeach
+
+                                    </table>
+                                </div>
+                                <div class="float-right">
+                                    {{ $users->withQueryString()->links() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/features-posts.js') }}"></script>
+@endpush
